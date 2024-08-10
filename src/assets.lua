74efbb4e246a8fa7e6d9b6e523ac59157631ce2c
UwU <mmimi0593@gmail.com>
2024-08-10 16:39:51 +0200

    Added a nil check to avoid crash on bad filenames on the asset manager

--- src/assets.lua
+++ src/assets.lua
@@ -20,40 +20,42 @@ local function getAtli(modId, textureScaling)
         blind = "Blind",
         sticker = "Sticker",
     }
-    local atliPath = "mods/" .. modId .. "/assets/textures/"..textureScaling.."x"
+    local atliPath = "mods/" .. modId .. "/assets/textures/" .. textureScaling .. "x"
     local dir = love.filesystem.getDirectoryItems(atliPath)
     for i, path in ipairs(dir) do
         local filename, extension = string.match(path, "([^/]+)%.(.+)$")
-        local name = modId .. "_" .. filename
-        local image = love.graphics.newImage(atliPath.."/"..path, {mipmaps = true, dpiscale = textureScaling})
-        local assetType = assetTypes[string.match(filename, "(.+)_")]
-        local px, py = 71, 95
-        if assetType == "Chip" then
-            px, py = 29, 29
-        end
-        if assetType == "Tag" then
-            px, py = 34, 34
-        end
-        if assetType == "Blind" then
-            px, py = 34, 34
-        end
-        if assetType == "Blind" then
-            table.insert(atli.animation, {
-                name = name,
-                image = image,
-                type = assetType,
-                frames = 21,
-                px = px,
-                py = py,
-            })
-        else
-            table.insert(atli.asset, {
-                name = name,
-                image = image,
-                type = assetType,
-                px = px,
-                py = py,
-            })
+        if filename ~= nil then
+            local name = modId .. "_" .. filename
+            local image = love.graphics.newImage(atliPath .. "/" .. path, { mipmaps = true, dpiscale = textureScaling })
+            local assetType = assetTypes[string.match(filename, "(.+)_")]
+            local px, py = 71, 95
+            if assetType == "Chip" then
+                px, py = 29, 29
+            end
+            if assetType == "Tag" then
+                px, py = 34, 34
+            end
+            if assetType == "Blind" then
+                px, py = 34, 34
+            end
+            if assetType == "Blind" then
+                table.insert(atli.animation, {
+                    name = name,
+                    image = image,
+                    type = assetType,
+                    frames = 21,
+                    px = px,
+                    py = py,
+                })
+            else
+                table.insert(atli.asset, {
+                    name = name,
+                    image = image,
+                    type = assetType,
+                    px = px,
+                    py = py,
+                })
+            end
         end
     end
     return atli
