ea03b1c0fec3d88efc50e8b5d428b0b0d4e1cc1b
UwU <mmimi0593@gmail.com>
2024-08-10 18:41:33 +0200

    Fixed local mod and mod file table merging

--- src/patches.lua
+++ src/patches.lua
@@ -3,10 +3,38 @@ local logging = require('logging')
 local utils = require('utils')
 local logger = logging.getLogger('patches')
 
+local function mergeTables(table1, table2)
+    local result = utils.copyTable(table1, true)
+    for k, v in pairs(table2) do
+        if type(v) == "table" and type(result[k]) == "table" then
+            local isDictTable = all(map(utils.keys(result), function(k)
+                return type(k) ~= "number"
+            end))
+            if isDictTable then
+                result[k] = mergeTables(result[k], v)
+            else
+                -- we assume it's an array table
+                for _, v2 in ipairs(v) do
+                    table.insert(result[k], v2)
+                end
+            end
+        else
+            result[k] = v
+        end
+    end
+    return result
+end
+
 logger:info("Loading mods from folders ")
 for _, mod in ipairs(balalib.get_local_mods()) do
     if mod ~= nil then
-        balamod.mods[mod.id] = mod
+        local modPath = "mods/" .. mod.id .. "/main"
+        local status, mod_table = pcall(require, modPath)
+        if not status then
+            logger:warn("Failed to load mod: ", modPath, " error: ", mod)
+        end
+
+        balamod.mods[mod.id] = mergeTables(mod, mod_table)
         logger:info("Loaded mod: ", mod.id)
     end
 end
