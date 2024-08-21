a6deab61fd225a77a7377c203821c56c29c5812a
UwU <mmimi0593@gmail.com>
2024-08-21 15:02:59 +0200

    Fixed asset images loading with ambiguous name

--- src/assets.lua
+++ src/assets.lua
@@ -1,5 +1,7 @@
 -- Asset loading API, in order to load assets from a mod's directory.
 local assets = {}
+local logging = require("logging")
+local logger = logging.getLogger("assets")
 
 local function getAtli(modId, textureScaling)
     local atli = {
@@ -22,12 +24,17 @@ local function getAtli(modId, textureScaling)
     }
     local atliPath = "mods/" .. modId .. "/assets/textures/" .. textureScaling .. "x"
     local dir = love.filesystem.getDirectoryItems(atliPath)
-    for i, path in ipairs(dir) do
-        local filename, extension = string.match(path, "([^/]+)%.(.+)$")
+    for _, path in ipairs(dir) do
+        local filename, _ = string.match(path, "([^/]+)%.(.+)$")
         if filename ~= nil then
             local name = modId .. "_" .. filename
             local image = love.graphics.newImage(atliPath .. "/" .. path, { mipmaps = true, dpiscale = textureScaling })
-            local assetType = assetTypes[string.match(filename, "(.+)_")]
+            local assetType = assetTypes[string.match(filename, "([^_]*)")]
+            if assetType == nil then
+                logger:warn("Unknown asset type for " .. filename)
+                assetType = "Unknown"
+            end
+
             local px, py = 71, 95
             if assetType == "Chip" then
                 px, py = 29, 29
